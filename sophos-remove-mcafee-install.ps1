df2e8634986bd39eef26c330d9d0ecdbd6a2278e
ghill <gareth.hill@cesa.catholic.edu.au>
2016-04-28 10:00:46 +0930

    Remove Sophos System Protection, removed redundant /passive flag, added /norestart flag to prevent this from triggering restarts on servers etc.

--- sophos-remove-mcafee-install.ps1
+++ sophos-remove-mcafee-install.ps1
@@ -12,36 +12,45 @@ if ((Get-Service -DisplayName "Sophos AutoUpdate Service" -ErrorAction SilentlyC
     $SophosUpdate64 = Get-ChildItem HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq "Sophos AutoUpdate"}
     if ($SophosUpdate32 -ne $null) {
         $UninstallGUID = $SophosUpdate32.PSChildName
-        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/passive", "/qn") -Wait
+        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
     }
     if ($SophosUpdate64 -ne $null) {
         $UninstallGUID = $SophosUpdate64.PSChildName
-        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/passive", "/qn") -Wait
+        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
     }
 
     $SophosRMS32 = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq "Sophos Remote Management System"}
     $SophosRMS64 = Get-ChildItem HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq "Sophos Remote Management System"}
     if ($SophosRMS32 -ne $null) {
         $UninstallGUID = $SophosRMS32.PSChildName
-        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/passive", "/qn") -Wait
+        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
     }
     if ($SophosRMS64 -ne $null) {
         $UninstallGUID = $SophosRMS64.PSChildName
-        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/passive", "/qn") -Wait
+        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
     }
 
     $SophosAV32 = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq "Sophos Anti-Virus"}
     $SophosAV64 = Get-ChildItem HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq "Sophos Anti-Virus"}
     if ($SophosAV32 -ne $null) {
         $UninstallGUID = $SophosAV32.PSChildName
-        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/passive", "/qn") -Wait
+        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
     }
     if ($SophosAV64 -ne $null) {
         $UninstallGUID = $SophosAV64.PSChildName
-        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/passive", "/qn") -Wait
+        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
     }
-
+    $SophosProt32 = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq "Sophos System Protection"}
+    $SophosProt64 = Get-ChildItem HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq "Sophos System Protection"}
+    if ($SophosProt32 -ne $null) {
+        $UninstallGUID = $SophosProt32.PSChildName
+        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
+    }
+    if ($SophosProt64 -ne $null) {
+        $UninstallGUID = $SophosProt64.PSChildName
+        Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
+    }
 }
-if (-Not (Get-Service -DisplayName "McAfee Agent Service" -ErrorAction SilentlyContinue)) {
+if ((-Not (Get-Service -DisplayName "McAfee Agent Service" -ErrorAction SilentlyContinue))) {
     Start-Process -FilePath $mcafeeinstallpath -ArgumentList @("-f", "-s") -Wait
 }
