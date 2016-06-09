19f20c4ff99e4be2dab254700ff27e18a435ca12
ghill <gareth.hill@cesa.catholic.edu.au>
2016-06-09 12:44:05 +0930

    Force Stop Services.

--- sophos-remove-mcafee-install.ps1
+++ sophos-remove-mcafee-install.ps1
@@ -1,4 +1,4 @@
-# v1.1
+# v1.2
 # Developed by Gareth Hill & Joshua Weller for Catholic Education South Australia
 # Accepts the path to the McAfee Installer as an argument
 # Removes any exists elements of a Sophos Anti-Virus Install, and triggers an installation of the McAfee (or any other) agent
@@ -10,6 +10,35 @@ if ((Get-Service -DisplayName "Sophos AutoUpdate Service" -ErrorAction SilentlyC
     foreach ($svc in Get-Service -DisplayName Sophos*) {
         Stop-Service -displayname $svc.DisplayName
     }
+    if (Test-Path 'C:\ProgramData\Sophos\Sophos Anti-Virus\Config\machine.xml') {
+       [xml]$sophosConfig = Get-Content 'C:\ProgramData\Sophos\Sophos Anti-Virus\Config\machine.xml'
+       if (($sophosConfig.configuration.components.TamperProtectionManagement.settings.enabled) -And ($sophosConfig.configuration.components.TamperProtectionManagement.settings.enabled -eq "true")) {
+          $sophosConfig.configuration.components.TamperProtectionManagement.settings.enabled = 'false'
+          $sophosConfig.Save('C:\ProgramData\Sophos\Sophos Anti-Virus\Config\machine.xml')
+          Start-Service -displayname "Sophos Anti-Virus" -Wait
+          Stop-Service -displayname "Sophos Anti-Virus"
+       }
+    }
+    Stop-Process -processname ALMon -force
+    Stop-Process -processname ManagementAgentNT -force
+    Stop-Process -processname SavService -force
+    Stop-Process -processname SAVAdminService -force
+    Stop-Process -processname Alsvc -force
+    Stop-Process -processname CertificationManagerServiceNT -force
+    Stop-Process -processname Sophos.FrontEnd.Service -force
+    Stop-Process -processname MgntSvc -force
+    Stop-Process -processname RouterNT -force
+    Stop-Process -processname PatchEndpointCommunicator -force
+    Stop-Process -processname PatchEndpointOrchestrator -force
+    Stop-Process -processname PatchServerCommunicator -force
+    Stop-Process -processname ssp -force
+    Stop-Process -processname SUMService -force
+    Stop-Process -processname swc_service -force
+    Stop-Process -processname swi_filter -force
+    Stop-Process -processname swi_service -force
+    Stop-Process -processname SntpService -force
+    Stop-Process -processname EnterpriseConsole -force
+    Stop-Process -processname Sophos.Messenger -force
     $SophosUpdate32 = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq "Sophos AutoUpdate"}
     $SophosUpdate64 = Get-ChildItem HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq "Sophos AutoUpdate"}
     if ($SophosUpdate32 -ne $null) {
@@ -62,6 +91,22 @@ if ((Get-Service -DisplayName "Sophos AutoUpdate Service" -ErrorAction SilentlyC
     if ($SophosNetProt64 -ne $null) {
         $UninstallGUID = $SophosNetProt64.PSChildName
         Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
+    }
+    $remaininginstalls = 0
+    foreach ($sophos32product in (Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -like "Sophos*"})) {
+        if ($sophos32product -ne $null) {
+            $remaininginstalls = $remaininginstalls + 1        
+        }
+    }
+    foreach ($sophos64product in (Get-ChildItem HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -like "Sophos*"})) {
+        if ($sophos64product -ne $null) {
+            $remaininginstalls = $remaininginstalls + 1
+        }
+    }
+    if ($remaininginstalls -le 4) {
+        foreach ($svc in Get-Service -DisplayName Sophos*) {
+            sc.exe delete $svc.Name
+        }
     }
 }
 if ((-Not (Get-Service -DisplayName "McAfee Agent Service" -ErrorAction SilentlyContinue))) {
